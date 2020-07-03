b7471f6d66584444b835ce1d4c74f47d656e85f3
hsk <robert@fingerprint.com.hk>
2020-07-04 00:05:02 +0800

    Updated edit goods

--- view-goods.php
+++ view-goods.php
@@ -21,87 +21,100 @@ $query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 
 if (mysqli_num_rows($query) > 0){
 	$goods = mysqli_fetch_assoc($query);
+
+	$availability = ($goods["status"] == 1) ? "Yes": "No";
+	print("
+	<div class='container'>
+	<div class='row'>
+	<div class='col-12 mt-2'>
+	<h4 class='text-secondary'>View Goods - $goods[goodsName]</h4>
+	</div>
+	</div>
+	</div>
+
+	<div class='container'>
+
+	<div class='row mt-2'>
+	<div class='col-12'>
+	<a href='list-goods.php' class='btn btn-default'>Return</a>
+	<a href='edit-goods.php?goodsNumber=$goods[goodsNumber]' class='btn btn-secondary'>Edit</a>
+	</div>
+	</div>
+
+	<div class='row mt-2'>
+	<div class='col-12 col-sm-3'>
+	ID
+	</div>
+
+	<div class='col-12 col-sm-8 text-secondary font-weight-bold'>
+	$goods[goodsNumber]
+	</div>
+	</div>
+
+	<div class='row mt-3'>
+	<div class='col-12 col-sm-3'>
+	Goods name
+	</div>
+	<div class='col-12 col-sm-8 text-secondary font-weight-bold'>
+	$goods[goodsName]
+	</div>
+	</div>
+
+	<div class='row mt-3'>
+	<div class='col-12 col-sm-3'>
+	Quantity
+	</div>
+	<div class='col-12 col-sm-8 text-secondary font-weight-bold'>
+	$goods[remainingStock]
+	</div>
+	</div>
+
+	<div class='row mt-3'>
+	<div class='col-12 col-sm-3'>
+	Stock Price
+	</div>
+	<div class='col-12 col-sm-8 text-secondary font-weight-bold'>
+	$goods[stockPrice]
+	</div>
+	</div>
+
+	<div class='row mt-3'>
+	<div class='col-12 col-sm-3'>
+	Store
+	</div>
+	<div class='col-12 col-sm-8 text-secondary font-weight-bold'>
+	$goods[ConsignmentStoreName]
+	</div>
+	</div>
+
+	<div class='row mt-3'>
+	<div class='col-12 col-sm-3'>
+	Available
+	</div>
+	<div class='col-12 col-sm-8 text-secondary font-weight-bold'>
+	$availability
+	</div>
+	</div>
+
+	</div>
+	");
+
 } else {
 	//todo
-	echo "Not found";
+
+	print("
+	<div class='container'>
+	<div class='row mt-2'>
+	<div class='col-12'>
+	<a href='list-goods.php' class='btn btn-default'>Return</a>
+	</div>
+	</div>
+	</div>
+	");
+
+	include_once "inc/404-content.php";
 }
 
-$availability = ($goods["status"] == 1) ? "Yes": "No";
-print("
-<div class='container'>
-	<div class='row'>
-		<div class='col-12 mt-2'>
-			<h4 class='text-secondary'>View Goods - $goods[goodsName]</h4>
-		</div>
-	</div>
-</div>
-
-<div class='container'>
-
-	<div class='row mt-2'>
-		<div class='col-12'>
-			<a href='list-goods.php' class='btn btn-default'>Return</a>
-			<a href='edit-goods.php?goodsNumber=$goods[goodsNumber]' class='btn btn-secondary'>Edit</a>
-		</div>
-	</div>
-
-	<div class='row mt-2'>
-		<div class='col-12 col-sm-3'>
-			ID
-		</div>
-
-		<div class='col-12 col-sm-8 text-secondary font-weight-bold'>
-			$goods[goodsNumber]
-		</div>
-	</div>
-
-	<div class='row mt-3'>
-		<div class='col-12 col-sm-3'>
-			Goods name
-		</div>
-		<div class='col-12 col-sm-8 text-secondary font-weight-bold'>
-			$goods[goodsName]
-		</div>
-	</div>
-
-	<div class='row mt-3'>
-		<div class='col-12 col-sm-3'>
-			Quantity
-		</div>
-		<div class='col-12 col-sm-8 text-secondary font-weight-bold'>
-			$goods[remainingStock]
-		</div>
-	</div>
-
-	<div class='row mt-3'>
-		<div class='col-12 col-sm-3'>
-			Stock Price
-		</div>
-		<div class='col-12 col-sm-8 text-secondary font-weight-bold'>
-			$goods[stockPrice]
-		</div>
-	</div>
-
-	<div class='row mt-3'>
-		<div class='col-12 col-sm-3'>
-			Store
-		</div>
-		<div class='col-12 col-sm-8 text-secondary font-weight-bold'>
-			$goods[ConsignmentStoreName]
-		</div>
-	</div>
-
-	<div class='row mt-3'>
-		<div class='col-12 col-sm-3'>
-			Available
-		</div>
-		<div class='col-12 col-sm-8 text-secondary font-weight-bold'>
-			$availability
-		</div>
-	</div>
-
-</div>
-");
 
 ?>
 
